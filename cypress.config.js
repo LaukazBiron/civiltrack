da17205ceff69f905eb4667dc65aec4b3faa9f8f
Limhi Lopez <88457245+LaukazBiron@users.noreply.github.com>
2026-07-02 22:49:00 -0600

    Video record cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,9 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
     apiUrl: "http://localhost:3000/api",
+    video: true,
+    videoCompression: 32,
+    trashAssetsBeforeRuns: true,
     reporter: "mochawesome",
     reporterOptions: {
       reportDir: "cypress/reports",
